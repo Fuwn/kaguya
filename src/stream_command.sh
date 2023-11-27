f88f3dbd4993625a4bc8f5a23593e09c83d5b6cf
Fuwn <contact@fuwn.me>
2023-11-26 18:51:04 -0800

    feat(stream): user agent flag

--- src/stream_command.sh
+++ src/stream_command.sh
@@ -1,11 +1,27 @@
+user_agent="${args['--user-agent']}"
+
+if [[ -n "${user_agent}" ]]; then
+	user_agent="--user-agent=${user_agent}"
+fi
+
 if [[ -n "${args['--download']}" ]]; then
 	START=$(date +%s)
 
-	yt-dlp --format 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best' --cookies-from-browser "${args[--browser]}" --all-subs --embed-subs "${args[uri]}"
+	yt-dlp \
+		--format 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best' \
+		--cookies-from-browser "${args[--browser]}" \
+		--all-subs \
+		--embed-subs \
+		"${user_agent}" \
+		"${args[uri]}"
 
 	printf "\ntook %s seconds\n" $(($(date +%s || true) - START))
 
 	return
 fi
 
-mpv --ytdl-raw-options-append=format='bestvideo[ext=mp4]+bestaudio[ext=m4a]/best' --ytdl-raw-options-append=cookies-from-browser="${args[--browser]}" "${args[uri]}"
+mpv \
+	--ytdl-raw-options-append=format='bestvideo[ext=mp4]+bestaudio[ext=m4a]/best' \
+	--ytdl-raw-options-append=cookies-from-browser="${args[--browser]}" \
+	"${user_agent}" \
+	"${args[uri]}"
